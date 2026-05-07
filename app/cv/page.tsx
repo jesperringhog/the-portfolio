be4f1d8611933e45d0375a932efadcde94b4318f
jesperringhog <jesper.ringhog@gmail.com>
2026-05-07 10:24:46 +0200

    changed title

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Cv() {
   return (
-    <section className="h-full flex flex-1 flex-col py-10 md:py-0">
+    <section className="h-full flex flex-1 flex-col">
       <iframe
         src="/cv/cv_jesper_ringhog_2026.pdf"
         className="w-full h-full"
